4414263b97acff3079c89fb341118a2329fc60f8
ajfrierson <alvinfrierson@fuller.edu>
2019-02-28 13:27:09 -0800

    added router to index.js.

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+import SignUp from './components/SignUp';
+import {Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/signup' component={SignUp} />
+    </div>
+  </Router>, document.getElementById('root'));
+registerServiceWorker();
