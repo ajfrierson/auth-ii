aad22dd70c68c1c172c567fe6c826e51f768f910
ajfrierson <alvinfrierson@fuller.edu>
2019-02-28 13:25:56 -0800

    added app component and registered service worker.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Authentication from './components/Authentication';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/users',{
+      headers: {
+        tokenKey: localStorage.getItem('token'),
+        userdep: localStorage.getItem('User Department')
+      }
+    }).then(response => {
+      this.setState({users: response.data})
+    }).catch(err => {
+      console.log('Ur error', err);
+    });
+  }
+
+  handleLogout() {
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+          <div>
+          {this.state.users.length > 0 ? this.state.users.map(user => {
+            return <div key={user.id}>{user.username}</div>
+          }):"Loading..."}
+          </div>
+          <div>
+          {<button onClick={this.handleLogout} className="logout-button">Log Out</button>}
+          </div>
       </div>
     );
   }
 }
 
-export default App;
+export default Authentication(App);
